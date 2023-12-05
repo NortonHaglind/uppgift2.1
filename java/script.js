e92470954756dfd1b7c870b22fcda76ac0830e3f
NortonHaglind <norton.haglindhilbers@elev.ga.ntig.se>
2023-12-05 10:57:28 +0100

    first commit

---
+++ java/script.js
@@ -0,0 +1,10 @@
+console.log("hejsan världen")
+const btnHello = document.getElementById("demo")
+const btn = document.getElementById("button")
+function myFunctionn() {
+    btnHello.innerHTML = "afafafafafa";
+    btn.classList.toggle("newbackground")
+  }
+function myFunction() {
+alert("ALERT! ALERT! ALERT! ALERT! ALERT!");
+}
